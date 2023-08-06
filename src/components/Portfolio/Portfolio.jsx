7a32f52d3437d363249bab7ef7a3aade8b0a7136
Babloo Kannojia <71953701+BablooKannojia@users.noreply.github.com>
2023-08-06 10:07:31 +0530

    Portfolio.jsx
    changes images

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -6,7 +6,12 @@ import Project1 from "../../img/p1.png";
 import Project2 from "../../img/p2.png";
 import Project3 from "../../img/p3.png";
 import Project4 from "../../img/p4.png";
+import Project5 from "../../img/p5.png";
+import Project6 from "../../img/p6.png";
+import Project7 from "../../img/p7.png";
+
 import { themeContext } from "../../Context";
+import { Link } from "react-scroll";
 const Portfolio = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
@@ -24,16 +29,24 @@ const Portfolio = () => {
         className="portfolio-slider"
       >
         <SwiperSlide>
-          <img src={Project1} alt="" />
+          <Link to="vnfashions.com">
+           <img src={Project1} alt="" />
+          </Link>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Project2} alt="" />
+          <Link to="">
+           <img src={Project2} alt="" />
+          </Link>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Project3} alt="" />
+          <Link to="">
+           <img src={Project3} alt="" />
+          </Link>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Project4} alt="" />
+          <Link to="">
+           <img src={Project4} alt="" />
+          </Link>
         </SwiperSlide>
       </Swiper>
     </div>
